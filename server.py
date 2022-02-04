c6de578ea970af39291ef066e64340305ab23e62
Mukul Shaunik <shaunikmail@gmail.com>
2022-02-04 07:09:11 +0000

    working

--- server.py
+++ server.py
@@ -12,18 +12,20 @@ app = Flask(__name__)
 @app.route("/save",methods = ["GET"])  
 def saveDetails():
 	try:  
+		print(request.args)
 		id = request.args.get('id')
 		temp = request.args.get('temp')
+		humid = request.args.get('humid')
 		app.logger.info(id)
 		app.logger.info(temp)
 		with sqlite3.connect("employee.db") as con:  
 			cur = con.cursor()  
-			app.logger.info('''insert into Temperature (name, temp, time) values
-				("{id}",{temp}, {time})
-				'''.format(id = id, temp = temp, time = round(time.time() * 1000)))
-			cur.execute('''insert into Temperature (name, temp, time) values
-				("{id}",{temp}, {time})
-				'''.format(id = id, temp = temp, time = round(time.time() * 1000)))  
+			app.logger.info('''insert into Temperature (name, temp, humidity, time) values
+				("{id}",{temp}, {humid}, {time})
+				'''.format(id = id, temp = temp, humid=humid, time = round(time.time() * 1000)))
+			cur.execute('''insert into Temperature (name, temp, humidity, time) values
+				("{id}",{temp}, {humid}, {time})
+				'''.format(id = id, temp = temp, humid=humid, time = round(time.time() * 1000)))  
 			con.commit()
 	except:  
 		traceback.print_exc()
@@ -36,19 +38,27 @@ def saveDetails():
 @app.route("/temp",methods = ["GET"])  
 def getTemp():
 	rows = ""
+	data = []
 	try:  
 		id = request.args.get('id')
+		limit = request.args.get('limit')
 		app.logger.info(id)
 		with sqlite3.connect("employee.db") as con:  
 			cur = con.cursor()  
-			cur.execute("select temp from Temperature where id = {id}".format(id = id))
+			cur.execute("select id, temp, humidity, time from Temperature where name = {id} order by time desc limit {limit}".format(id = id, limit=limit))
 			rows = cur.fetchall()
-			app.logger.info(rows)
+			
+			for r in rows:
+				if(r[0] is None or r[1] is None or r[2] is None or r[3] is None):
+					continue
+				innerData = {"id":r[0], "temperature": r[1], "humidity": r[2], "time": r[3]}
+				data.append(innerData)
+			
 	except:
 		traceback.print_exc()
 		pass
-	finally: 
-		return jsonify(rows[0][0]) 
+	finally:  
+		return jsonify(data)
 		con.close() 
 
 if __name__ == "__main__":  
--- test_conn/test_conn.ino
+++ test_conn/test_conn.ino
@@ -1,9 +1,10 @@
- #include <SoftwareSerial.h>
- #include <dht.h>
+#include <SoftwareSerial.h>
+#include <dht.h>
 
 dht DHT;
 
 #define DHT11_PIN 7
+
 
 SoftwareSerial SIM900(9, 10); //TX|RX of sim900 a to connected to 9|10
 
@@ -14,68 +15,82 @@ void setup()
   delay(100);
 }
 
-double getTemp(){
-    int chk = DHT.read11(DHT11_PIN);
-    return DHT.temperature;
+void delayFun(){
+  delay(3000);
+  }
+void delaySlowerFun(){
+  delay(7000);
   }
 
 void send() {
-  Serial.println("HTTP post method :");
-  Serial.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\\r\\n");
+  int chk = DHT.read11(DHT11_PIN);
+  double temp,humid;
+  Serial.print("Temperature = ");
+  temp = DHT.temperature;
+  humid = DHT.humidity; 
+  Serial.println(temp);
+  Serial.print("Humidity = ");
+  Serial.println(humid);
+  Serial.println("HTTP get method :");
+  Serial.println("AT\\r\\n");
+  SIM900.println("AT");  /* Check Communication */
+  delayFun();
+  ShowSerialData(); /* Print response on the serial monitor */
+  /* Configure bearer profile 1 */
+  Serial.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\\r\\n");    
   SIM900.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");  /* Connection type GPRS */
+  delayFun();
   ShowSerialData();
-  
-  Serial.println("AT+SAPBR=3,1,\"APN\",\"bsnlnet\"\\r\\n");
+  Serial.println("AT+SAPBR=3,1,\"APN\",\"bsnlnet\"\\r\\n");  
   SIM900.println("AT+SAPBR=3,1,\"APN\",\"bsnlnet\"");  /* APN of the provider */
+  delayFun();
   ShowSerialData();
-  
-  //close gprs context just in case
-  Serial.println("AT+SAPBR=0,1\\r\\n");
-  SIM900.println("AT+SAPBR=0,1"); /* close GPRS context */
-  ShowSerialData();
-  
   Serial.println("AT+SAPBR=1,1\\r\\n");
   SIM900.println("AT+SAPBR=1,1"); /* Open GPRS context */
+  delayFun();
   ShowSerialData();
-  
   Serial.println("AT+SAPBR=2,1\\r\\n");
   SIM900.println("AT+SAPBR=2,1"); /* Query the GPRS context */
+  delayFun();
   ShowSerialData();
-
-  httpCall();
-  
-}
-
-void httpCall(){
-//  Serial.println("AT+HTTPINIT\\r\\n");
-//  SIM900.println("AT+HTTPINIT");  /* Initialize HTTP service */
-//  ShowSerialData();
-//
-  Serial.println("AT+HTTPSSL=1\\r\\n");
-  SIM900.println("AT+HTTPSSL=1");  /* set to  ssl */
+  Serial.println("AT+HTTPINIT\\r\\n");
+  SIM900.println("AT+HTTPINIT"); /* Initialize HTTP service */
+  delayFun(); 
   ShowSerialData();
-  
   Serial.println("AT+HTTPPARA=\"CID\",1\\r\\n");
   SIM900.println("AT+HTTPPARA=\"CID\",1");  /* Set parameters for HTTP session */
+  delayFun();
   ShowSerialData();
-  
-  Serial.println("AT+HTTPPARA=\"URL\",\"http://0637-103-40-196-20.ngrok.io\"\\r\\n");
-  SIM900.println("AT+HTTPPARA=\"URL\",\"0637-103-40-196-20.ngrok.io/save?id=1&temp="+String(getTemp(),2)+"\"");  /* Set parameters for HTTP session */
+  delayFun();
+  Serial.println("AT+HTTPPARA=\"URL\",\"0637-103-40-196-20.ngrok.io\"\\r\\n");
+  SIM900.println("AT+HTTPPARA=\"URL\",\"0637-103-40-196-20.ngrok.io/save?id=1&temp=" + String(temp,2) + "&humid="+ String(humid,2) +"\"");  /* Set parameters for HTTP session */
+  delayFun();
   ShowSerialData();
-  
   Serial.println("AT+HTTPACTION=0\\r\\n");
-  SIM900.println("AT+HTTPACTION=0");  /* Start POST session */
+  SIM900.println("AT+HTTPACTION=0");  /* Start GET session */
+  delaySlowerFun();
   ShowSerialData();
-  
   Serial.println("AT+HTTPREAD\\r\\n");
-  SIM900.println("AT+HTTPREAD");  /* Start POST session */\
+  SIM900.println("AT+HTTPREAD");  /* Read data from HTTP server */
+  delaySlowerFun();
   ShowSerialData();
-  
-  delay(10000);
-  }
-
+  Serial.println("AT+HTTPTERM\\r\\n");  
+  SIM900.println("AT+HTTPTERM");  /* Terminate HTTP service */
+  delayFun();
+  ShowSerialData();
+  Serial.println("AT+SAPBR=0,1\\r\\n");
+  SIM900.println("AT+SAPBR=0,1"); /* Close GPRS context */
+  delayFun();
+  ShowSerialData();
+}
 
 void ShowSerialData()
+{
+  while(SIM900.available()!=0)  /* If data is available on serial port */
+  Serial.write(char (SIM900.read())); /* Print character received on to the serial monitor */
+}
+
+void ShowSerialData1()
 {
   wait("OK,ERROR",10000);
   Serial.println();
